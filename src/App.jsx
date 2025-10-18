390ca98d8e9338f86df3f41dde5316f7822243df
Zhurkin-Alex <jura-leha-999@mail.ru>
2025-10-18 17:59:27 +0300

    text

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
       <div className="rounded-2xl bg-white p-8 text-slate-800 shadow-xl">
         <h1 className="mb-2 text-2xl font-bold">Hello, Preact + Tailwind + Webpack! ✨</h1>
         <p className="opacity-80">
-          Стартовый шаблон готов. Редактируй <code>src/App.jsx</code>.
+          Стартовый шаблон готов. Редактируй <code>src/App.jsx!</code>.
         </p>
       </div>
     </main>
